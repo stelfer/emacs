923d7923c9e0c9512ba473ccaa2932ee92b432d2
Soren Telfer <soren.telfer@gmail.com>
2016-01-10 08:07:05 -0800

    Removed desktop+

--- elpa/desktop+-20151004.1240/desktop+-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "desktop+" "20151004.1240" "Handle special buffers when saving & restoring sessions" '((emacs "24.4") (dash "2.11.0") (f "0.17.2")) :url "https://github.com/ffevotte/desktop-plus")
