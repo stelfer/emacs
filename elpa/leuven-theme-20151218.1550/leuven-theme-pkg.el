831e1c0ef1cb7295dfec847e6adf10fd6499579c
Soren Telfer <soren.telfer@gmail.com>
2016-01-25 09:04:32 -0800

    Updated elpa

--- elpa/leuven-theme-20151218.1550/leuven-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "leuven-theme" "20151218.1550" "Awesome Emacs color theme on white background" 'nil :url "https://github.com/fniessen/emacs-leuven-theme" :keywords '("color" "theme"))
