35150cf23a457d4cb93c2e3605f1a0b30adf039e
Soren Telfer <soren.telfer@gmail.com>
2016-01-25 09:18:43 -0800

    Emacs adds custom-set-faces stanza by default

--- init.el
+++ init.el
@@ -542,3 +542,9 @@ enter missing field manually."
 (defun rtags-load-database (dir)
   (rtags-call-rc "-J" dir))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
