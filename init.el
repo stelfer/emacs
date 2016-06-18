ece7d59f1267b69ad44927ecb6c37d7ccee015ed
Soren Telfer <soren.telfer@gmail.com>
2016-06-17 17:51:34 -0700

    Added modern-c++-mode

--- init.el
+++ init.el
@@ -283,24 +283,24 @@
 	    
 	    (setq yas-indent-line 'fixed)))
 
-(add-hook 'c++-mode-hook
-	  '(lambda()
-	     (font-lock-add-keywords
-	      nil '(;; complete some fundamental keywords
-		    ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
-		    ;; add the new C++11 keywords
-		    ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
-		    ("\\<\\(char[0-9]+_t\\)\\>" . font-lock-keyword-face)
-		    ;; PREPROCESSOR_CONSTANT
-		    ("\\<[A-Z]+[A-Z_]+\\>" . font-lock-constant-face)
-		    ;; hexadecimal numbers
-		    ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
-		    ;; integer/float/scientific numbers
-		    ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
-		    ;; user-types (customize!)
-		    ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(t\\|type\\|ptr\\)\\>" . font-lock-type-face)
-		    ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)))
-	     ) t)
+(add-hook 'c++-mode-hook #'modern-c++-font-lock-mode)
+	  ;; '(lambda()
+	  ;;    (font-lock-add-keywords
+	  ;;     nil '(;; complete some fundamental keywords
+	  ;; 	    ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
+	  ;; 	    ;; add the new C++11 keywords
+	  ;; 	    ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
+	  ;; 	    ("\\<\\(char[0-9]+_t\\)\\>" . font-lock-keyword-face)
+	  ;; 	    ;; PREPROCESSOR_CONSTANT
+	  ;; 	    ("\\<[A-Z]+[A-Z_]+\\>" . font-lock-constant-face)
+	  ;; 	    ;; hexadecimal numbers
+	  ;; 	    ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
+	  ;; 	    ;; integer/float/scientific numbers
+	  ;; 	    ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
+	  ;; 	    ;; user-types (customize!)
+	  ;; 	    ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(t\\|type\\|ptr\\)\\>" . font-lock-type-face)
+	  ;; 	    ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)))
+	  ;;    ) t)
 
 
 (defun my-rtags-create-doxygen-comment ()
